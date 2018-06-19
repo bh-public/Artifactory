0c62732682b6043a80559bf1211ba7b3aa6d1c0b
bh-public <bh.public@comcast.net>
2018-06-19 12:27:03 -0700

    remove Cisco lab Proxy settings

--- jfrog_install.sh
+++ jfrog_install.sh
@@ -5,7 +5,7 @@ exec > >(tee -a /var/tmp/jfrog-node-init_$$.log) 2>&1
 . /usr/local/osmosix/service/utils/agent_util.sh
 
 #Declaring variable used in the script
-PROXY_ADDRESS="http://proxy-wsa.esl.cisco.com:80"
+#PROXY_ADDRESS="http://proxy-wsa.esl.cisco.com:80"
 
 #Script used for Jfrog Artifactory Server Service
 # In case you have a local CloudCenter repository, disable it
@@ -19,7 +19,8 @@ else
   agentSendLogMessage "Artifactory pre-requisuite checks ok"
 fi
 
-sudo wget wget -e https_proxy=$PROXY_ADDRESS https://bintray.com/jfrog/artifactory-rpms/rpm -O bintray-jfrog-artifactory-rpms.repo
+#sudo wget wget -e https_proxy=$PROXY_ADDRESS https://bintray.com/jfrog/artifactory-rpms/rpm -O bintray-jfrog-artifactory-rpms.repo
+sudo wget -e https://bintray.com/jfrog/artifactory-rpms/rpm -O bintray-jfrog-artifactory-rpms.repo
 sudo mv bintray-jfrog-artifactory-rpms.repo /etc/yum.repos.d/
 
 # Warning. We are going to install version 5.11. Latest versions works fine as well but we need to fix a Cross-Site Request Forgery for admin 
